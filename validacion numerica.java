e1f54bf60907de92351132f6d2dd417cb341c049
Harrycastillo0 <harry_jafet@Unitec.edu>
2026-04-26 19:55:27 -0600

    Add files via upload

---
+++ validacion numerica.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Validacionnumeros {
+
+    public static void main(String args[]) {
+
+        Scanner leer = new Scanner(System.in);
+
+        int numero;
+
+        System.out.print("Ingrese un número entero: ");
+
+        numero = leer.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El número es par");
+        } else {
+            System.out.println("El número es impar");
+        }
+    }
+}
