41c438867b35776b5d8efe053b74ffbf199b201d
Harrycastillo0 <harry_jafet@Unitec.edu>
2026-04-26 19:50:28 -0600

    Add files via upload

---
+++ semaforo.java
@@ -0,0 +1,14 @@
+public class Semaforo {
+
+    static String verde = "Puede avanzar";
+    static String amarillo = "Avance con precaución";
+    static String rojo = "Debe detenerse";
+
+    public static void main(String[] args) {
+
+        System.out.println("Verde: " + verde);
+        System.out.println("Amarillo: " + amarillo);
+        System.out.println("Rojo: " + rojo);
+
+    }
+}
